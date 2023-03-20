656ac535a28792a75a157c5aa367bd542bd8146c
kevindiaz15 <polsion155@gmail.com>
2023-03-20 10:47:01 -0500

    autenticacion

---
+++ src/autenticacion.js
@@ -0,0 +1,11 @@
+var nombreUsuario = "kevin15.";
+
+function autenticacion(usuario) {
+    usuario;
+    if (nombreUsuario===usuario) {
+        alert ("autenticacion correcta")
+        }else (
+            alert("autenticaicon incorrecta")
+        )
+}
+autenticacion(prompt("ingrese usuario"))
