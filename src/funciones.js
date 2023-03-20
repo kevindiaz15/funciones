16c79f0fb0a9c66eed19847d20e76b202abf76cf
kevindiaz15 <polsion155@gmail.com>
2023-03-20 10:35:44 -0500

    cambio de contraseña

---
+++ src/funciones.js
@@ -0,0 +1,8 @@
+
+
+function cambiarContraseña(newcontra) {
+    newcontra;
+    alert ("nueva contraseña ingresada exitosamente");
+    console.log(newcontra);
+}
+cambiarContraseña(prompt("ingrese contraseña"));
