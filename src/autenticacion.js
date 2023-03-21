887d304e91ee4d9f8c74fa8923e60af1cddb72ff
kevindiaz15 <polsion155@gmail.com>
2023-03-20 20:09:19 -0500

    arreglo

--- src/autenticacion.js
+++ src/autenticacion.js
@@ -3,9 +3,9 @@ var nombreUsuario = "kevin15.";
 function autenticacion(usuario) {
     usuario;
     if (nombreUsuario===usuario) {
-        alert ("autenticacion correcta")
+        alert ("autenticacion correcta");
         }else (
-            alert("autenticaicon incorrecta")
+            alert("autenticaicon incorrecta");
         )
 }
-autenticacion(prompt("ingrese usuario"))
+autenticacion(prompt("ingrese usuario"));
